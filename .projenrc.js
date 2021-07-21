2801fed983dc7e8c91a0ecb13588d707329a9864
Neil Kuan <guan840912@gmail.com>
2021-07-21 15:02:59 +0800

    chore: project created with projen

---
+++ .projenrc.js
@@ -0,0 +1,7 @@
+const { AwsCdkTypeScriptApp } = require('projen');
+const project = new AwsCdkTypeScriptApp({
+  cdkVersion: '1.95.2',
+  defaultReleaseBranch: 'main',
+  name: 'cdk-cloudformation-guard-demo',
+});
+project.synth();
